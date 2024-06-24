c26ecc2860738ca87c3eaf6527478e622b6dc613
Saddss <108515797+Saddss@users.noreply.github.com>
2024-06-24 20:01:21 +0800

    优化用户注册,高并发情况下多相同用户名注册时发生主键冲突进行异常捕获并抛出自定义异常

--- admin/src/main/java/com/saddss/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/saddss/shortlink/admin/service/impl/UserServiceImpl.java
@@ -22,6 +22,7 @@ import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -62,8 +63,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         RLock lock = redissonClient.getLock(RedisCacheConstant.LOCK_USER_REGISTER_KEY + requestParam.getUsername());
         try {
             if (lock.tryLock()){
-                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-                if (inserted < 1){
+                try {
+                    baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                } catch (DuplicateKeyException ex) {
                     throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
                 }
                 userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
