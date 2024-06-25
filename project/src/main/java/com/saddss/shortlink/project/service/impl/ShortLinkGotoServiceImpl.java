9a04b1752ab4104aed85b9faa3d7209d8968aa41
Saddss <108515797+Saddss@users.noreply.github.com>
2024-06-25 19:56:22 +0800

    初始化短链接跳转表相关代码，完成分表配置

---
+++ project/src/main/java/com/saddss/shortlink/project/service/impl/ShortLinkGotoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.saddss.shortlink.project.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.saddss.shortlink.project.dao.entity.ShortLinkGotoDO;
+import com.saddss.shortlink.project.dao.mapper.ShortLinkGotoMapper;
+import com.saddss.shortlink.project.service.ShortLinkGotoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShortLinkGotoServiceImpl extends ServiceImpl<ShortLinkGotoMapper, ShortLinkGotoDO> implements ShortLinkGotoService {
+}
