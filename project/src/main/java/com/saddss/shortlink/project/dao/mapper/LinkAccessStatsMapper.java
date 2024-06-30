b778baf4bb15e371cd4097244937c5e854e7a537
Saddss <108515797+Saddss@users.noreply.github.com>
2024-06-30 19:18:33 +0800

    插入访问记录时更新updateTime为当前时间

--- project/src/main/java/com/saddss/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
+++ project/src/main/java/com/saddss/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
@@ -17,6 +17,7 @@ public interface LinkAccessStatsMapper extends BaseMapper<LinkAccessStatsDO> {
     @Insert("INSERT INTO t_link_access_stats (full_short_url, gid, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag) " +
             "VALUES( #{linkAccessStats.fullShortUrl}, #{linkAccessStats.gid}, #{linkAccessStats.date}, #{linkAccessStats.pv}, #{linkAccessStats.uv}, #{linkAccessStats.uip}, #{linkAccessStats.hour}, #{linkAccessStats.weekday}, NOW(), NOW(), 0) ON DUPLICATE KEY UPDATE pv = pv +  #{linkAccessStats.pv}, " +
             "uv = uv + #{linkAccessStats.uv}, " +
-            "uip = uip + #{linkAccessStats.uip};")
+            "uip = uip + #{linkAccessStats.uip}," +
+            "update_time = NOW();")
     void shortLinkStats(@Param("linkAccessStats") LinkAccessStatsDO linkAccessStatsDO);
 }
