d6a06ffc042047e12657c52891a4691cdca424d0
Saddss <108515797+Saddss@users.noreply.github.com>
2024-07-02 19:15:18 +0800

    完成跳转链接时访问网络记录的插入

--- project/src/main/java/com/saddss/shortlink/project/util/LinkUtil.java
+++ project/src/main/java/com/saddss/shortlink/project/util/LinkUtil.java
@@ -113,19 +113,19 @@ public class LinkUtil {
         return "PC";
     }
 
-//    /**
-//     * 获取用户访问网络
-//     *
-//     * @param request 请求
-//     * @return 访问设备
-//     */
-//    public static String getNetwork(HttpServletRequest request) {
-//        String actualIp = getActualIp(request);
-//        // 这里简单判断IP地址范围，您可能需要更复杂的逻辑
-//        // 例如，通过调用IP地址库或调用第三方服务来判断网络类型
-//        return actualIp.startsWith("192.168.") || actualIp.startsWith("10.") ? "WIFI" : "Mobile";
-//    }
-//
+    /**
+     * 获取用户访问网络
+     *
+     * @param request 请求
+     * @return 访问设备
+     */
+    public static String getNetwork(HttpServletRequest request) {
+        String actualIp = getActualIp(request);
+        // 这里简单判断IP地址范围，您可能需要更复杂的逻辑
+        // 例如，通过调用IP地址库或调用第三方服务来判断网络类型
+        return actualIp.startsWith("192.168.") || actualIp.startsWith("10.") ? "WIFI" : "Mobile";
+    }
+
 //    /**
 //     * 获取原始链接中的域名
 //     * 如果原始链接包含 www 开头的话需要去掉
