c3f906a3608bc481092ce0414328f66f60edfcfe
Saddss <108515797+Saddss@users.noreply.github.com>
2024-07-05 16:23:25 +0800

    修复创建分组失败bug，收录奇怪bug，可能是构造方法不全导致的。

--- admin/src/main/java/com/saddss/shortlink/admin/dto/req/ShortLinkGroupSaveReqDTO.java
+++ admin/src/main/java/com/saddss/shortlink/admin/dto/req/ShortLinkGroupSaveReqDTO.java
@@ -2,12 +2,14 @@ package com.saddss.shortlink.admin.dto.req;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 短链接分组创建参数
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ShortLinkGroupSaveReqDTO {
 
     /**
