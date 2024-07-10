02399ee075d3be48b4e7ab158a527318264b295d
Saddss <108515797+Saddss@users.noreply.github.com>
2024-07-10 16:55:36 +0800

    对后管服务进行流量控制，同一个用户在一秒内发起请求超过20次会报错。 @SneakyThrows注解可以避免在方法前面上显示抛出异常，也可以不用捕获异常，受检一场会转变为运行时异常抛出

--- admin/src/main/java/com/saddss/shortlink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/saddss/shortlink/admin/config/UserConfiguration.java
@@ -1,8 +1,9 @@
-
 package com.saddss.shortlink.admin.config;
 
 
+import com.saddss.shortlink.admin.common.biz.user.UserFlowRiskControlFilter;
 import com.saddss.shortlink.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,18 +39,18 @@ public class UserConfiguration {
 //        return registration;
 //    }
 
-//    /**
-//     * 用户操作流量风控过滤器
-//     */
-//    @Bean
-//    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
-//    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
-//            StringRedisTemplate stringRedisTemplate,
-//            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
-//        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
-//        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
-//        registration.addUrlPatterns("/*");
-//        registration.setOrder(10);
-//        return registration;
-//    }
+    /**
+     * 用户操作流量风控过滤器
+     */
+    @Bean
+    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
+    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
+            StringRedisTemplate stringRedisTemplate,
+            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(10);
+        return registration;
+    }
 }
