9c23a1c886eab04d51ea9ba9d55777883c117e66
Saddss <108515797+Saddss@users.noreply.github.com>
2024-07-11 21:24:57 +0800

    引入nacos，将后管和中台服务注册到注册中心

--- admin/src/main/java/com/saddss/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/com/saddss/shortlink/admin/ShortLinkAdminApplication.java
@@ -3,9 +3,11 @@ package com.saddss.shortlink.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.saddss.shortlink.admin.dao.mapper")
+@EnableDiscoveryClient
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkAdminApplication.class, args);
--- project/src/main/java/com/saddss/shortlink/project/ShortLinkProjectApplication.java
+++ project/src/main/java/com/saddss/shortlink/project/ShortLinkProjectApplication.java
@@ -3,9 +3,11 @@ package com.saddss.shortlink.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.saddss.shortlink.project.dao.mapper")
+@EnableDiscoveryClient
 public class ShortLinkProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkProjectApplication.class, args);
